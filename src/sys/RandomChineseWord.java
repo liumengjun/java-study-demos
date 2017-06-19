383f1ca0d2d95004f6636f0f8adcf9d32059e750
Mengjun Liu <mengjun_liu@126.com>
2017-06-19 18:02:19 +0800

    ignore generated files

--- src/sys/RandomChineseWord.java
+++ src/sys/RandomChineseWord.java
@@ -1,18 +1,26 @@
 package sys;
 
-
 import java.io.PrintWriter;
 
+/**
+ * 输出汉字，文件编码GBK
+ */
 public class RandomChineseWord {
 	public static void main(String[] args) {
 		char ch;
-		try{
-			PrintWriter writer=new PrintWriter("text/汉字.txt");
-			for(int i=19968;i<41870;i++){
-				ch = (char)i;
-				writer.print(i+":\t"+ch+",\t");
+		try {
+			PrintWriter writer = new PrintWriter("text/汉字.txt");
+			for (int i = 19968, j = 1; i < 40870; i++, j++) {
+				ch = (char) i;
+				writer.print(i + ":\t" + ch + ",\t");
+				if (j == 16) {
+					writer.print("\n");
+					j = 0;
+				}
 			}
-		}catch (Exception e) {
+			writer.close();
+			System.out.println("请看\"text/汉字.txt\"文件。");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
