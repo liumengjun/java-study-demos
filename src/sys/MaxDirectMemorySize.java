57c5dd6d15df089e009badf63dca8f929da3464a
Mengjun Liu <mengjun_liu@126.com>
2017-06-21 10:54:23 +0800

    获取maxDirectMemory信息

---
+++ src/sys/MaxDirectMemorySize.java
@@ -0,0 +1,36 @@
+package sys;
+
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
+
+/**
+ * -Xmx256 -XX:MaxDirectMemorySize=100M -Dsun.nio.MaxDirectMemorySize=50000000<br>
+ * or -Xmx256<br>
+ * NOTE: 指定-Dsun.nio.MaxDirectMemorySize没有其作用
+ * 
+ * @author liumengjun
+ *
+ */
+public class MaxDirectMemorySize {
+
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, SecurityException,
+	        IllegalArgumentException, IllegalAccessException {
+		System.out.println("maxDirectMemory:" + sun.misc.VM.maxDirectMemory());
+
+		System.out.println("================================");
+
+		ByteBuffer buffer = ByteBuffer.allocateDirect(0);
+		Class<?> c = Class.forName("java.nio.Bits");
+		Field maxMemory = c.getDeclaredField("maxMemory");
+		maxMemory.setAccessible(true);
+		synchronized (c) {
+			Long maxMemoryValue = (Long) maxMemory.get(null);
+			System.out.println("java.nio.Bits.maxMemory:" + maxMemoryValue);
+		}
+
+		System.out.println("================================");
+
+		System.out.println("Runtime.maxMemory:" + Runtime.getRuntime().maxMemory());
+
+	}
+}
