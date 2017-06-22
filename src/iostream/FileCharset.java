699a2cf968237ccd050e8ed662c8d72dc7268a73
Mengjun Liu <mengjun_liu@126.com>
2017-06-22 23:10:03 +0800

    reverse file content

--- src/iostream/FileCharset.java
+++ src/iostream/FileCharset.java
@@ -3,6 +3,7 @@ package iostream;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.charset.Charset;
 
 public class FileCharset {
@@ -16,7 +17,7 @@ public class FileCharset {
 	 */
 	public static String codeString(String fileName) throws Exception {
 		BufferedInputStream bin = new BufferedInputStream(new FileInputStream(
-				fileName));
+		        fileName));
 		int p = (bin.read() << 8) + bin.read();
 		String code = null;
 		// 其中的 0xefbb、0xfffe、0xfeff、0x5c75这些都是这个文件的前面两个字节的16进制数
@@ -44,10 +45,10 @@ public class FileCharset {
 	public static String get_charset(File file) {
 		String charset = "GBK";
 		byte[] first3Bytes = new byte[3];
+		BufferedInputStream bis = null;
 		try {
 			boolean checked = false;
-			BufferedInputStream bis = new BufferedInputStream(
-					new FileInputStream(file));
+			bis = new BufferedInputStream(new FileInputStream(file));
 			bis.mark(0);
 			int read = bis.read(first3Bytes, 0, 3);
 			if (read == -1)
@@ -56,20 +57,20 @@ public class FileCharset {
 				charset = "UTF-16LE";
 				checked = true;
 			} else if (first3Bytes[0] == (byte) 0xFE
-					&& first3Bytes[1] == (byte) 0xFF) {
+			        && first3Bytes[1] == (byte) 0xFF) {
 				charset = "UTF-16BE";
 				checked = true;
 			} else if (first3Bytes[0] == (byte) 0xEF
-					&& first3Bytes[1] == (byte) 0xBB
-					&& first3Bytes[2] == (byte) 0xBF) {
+			        && first3Bytes[1] == (byte) 0xBB
+			        && first3Bytes[2] == (byte) 0xBF) {
 				charset = "UTF-8";
 				checked = true;
 			}
-			bis.reset();
 			if (!checked) {
 				int len = 0;
 				int loc = 0;
 
+				bis.reset();
 				while ((read = bis.read()) != -1) {
 					loc++;
 					if (read >= 0xF0)
@@ -79,8 +80,8 @@ public class FileCharset {
 					if (0xC0 <= read && read <= 0xDF) {
 						read = bis.read();
 						if (0x80 <= read && read <= 0xBF)// 双字节 (0xC0 - 0xDF)
-															// (0x80 -
-															// 0xBF),也可能在GB编码内
+						                                 // (0x80 -
+						                                 // 0xBF),也可能在GB编码内
 							continue;
 						else
 							break;
@@ -97,23 +98,28 @@ public class FileCharset {
 							break;
 					}
 				}
-				System.out.println(loc + " " + Integer.toHexString(read));
+				// System.out.println(loc + " " + Integer.toHexString(read));
 			}
 
-			bis.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (bis != null)
+					bis.close();
+			} catch (IOException e) {
+			}
 		}
 
 		return charset;
 	}
 
 	public static void main(String[] args) throws Exception {
-		
+
 		Charset defaultCharset = Charset.defaultCharset();
 		System.out.println(defaultCharset);
-		
-		String file_name = "text/text_file2.txt";
+
+		String file_name = "text/text_file.txt";
 		String charset_name = get_charset(new File(file_name));
 		System.out.println(charset_name);
 		charset_name = codeString(file_name);
---
+++ src/iostream/ReverseFile.java
@@ -0,0 +1,82 @@
+package iostream;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ReverseFile {
+
+	private static class CharArrayWrapper {
+		private char[] buf;
+		private int len;
+
+		private CharArrayWrapper(char[] buf, int len) {
+			this.buf = buf.clone();
+			this.len = len;
+		}
+
+		private CharArrayWrapper reverse() {
+			for (int i = 0; i < len / 2; i++) {
+				char c = buf[i];
+				buf[i] = buf[len - 1 - i];
+				buf[len - 1 - i] = c;
+			}
+			return this;
+		}
+	}
+
+	public static void reverseFile(File srcFile, File destFile) {
+		String charset = FileCharset.get_charset(srcFile);
+		final int BUF_SIZE = 256;
+		char[] cbuf = new char[BUF_SIZE];
+		BufferedReader reader = null;
+		BufferedWriter writer = null;
+		try {
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(srcFile), charset));
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(destFile), charset));
+			// read into bufs
+			LinkedList<CharArrayWrapper> bufs = new LinkedList<>();
+			int len = -1;
+			while ((len = reader.read(cbuf)) != -1) {
+				bufs.add(new CharArrayWrapper(cbuf, len));
+			}
+			System.out.println("read times: " + bufs.size());
+
+			// write from bufs
+			for (Iterator<CharArrayWrapper> itr = bufs.descendingIterator(); itr.hasNext();) {
+				CharArrayWrapper bw = itr.next().reverse();
+				// System.out.println(new String(bw.buf, 0, bw.len));
+				writer.write(bw.buf, 0, bw.len);
+			}
+			writer.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+				}
+			}
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		File srcFile = new File("text/text_file2.txt");
+		File destFile = new File("temp/reverse_text_file2.txt");
+		reverseFile(srcFile, destFile);
+	}
+}
